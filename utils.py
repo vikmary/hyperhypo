e8d512eae0b682697352a53fb12556a5bd47aa13
Mariia Trofimova <mary.vikhreva@gmail.com>
2020-01-13 12:03:07 +0300

    feat: add loading of hyperstar data

--- utils.py
+++ utils.py
@@ -8,11 +8,51 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 
 
+def synsets2senses(synsets: dict) -> List[dict]:
+    """Converts dict of synsets to senses."""
+    senses = []
+    for synset in synsets.values():
+        for s in synset['senses']:
+            senses.append(s)
+            if 'hypernyms' in s:
+                senses[-1]['hypernyms'] = s['hypernyms']
+    return senses
+
+
+def get_test_senses(fpaths: Iterator[Union[str, Path]]) -> List[dict]:
+    """Gets test senses with their texts."""
+    senses = []
+    for fp in fpaths:
+        sys.stderr.write(f"Parsing {fp}.\n")
+        for row in open(fp, 'rt'):
+            senses.append({'content': row.strip()})
+    return senses
+
+
+def get_train_synsets(fpaths: Iterator[Union[str, Path]]) -> Dict:
+    """Gets synsets with id as key, senses and hyperonyms as values."""
+    synsets_dict = {}
+    for fp in fpaths:
+        sys.stderr.write(f"Parsing {fp}.\n")
+        with open(fp, 'rt') as fin:
+            fin.readline()
+            for row in fin:
+                synset_id, senses, hyper_synset_ids = row.rstrip().split('\t', 2)
+                senses = senses.split(',')
+                hyper_synset_ids = hyper_synset_ids.split(',')
+                if synset_id in synsets_dict:
+                    raise ValueError(f"multiple synsets with id = \'{synset_id}\'")
+                synsets_dict[synset_id] = {'senses': [{'content': s} for s in senses],
+                                           'hypernyms': [{'id': i}
+                                                         for i in hyper_synset_ids]}
+    return synsets_dict
+
+
 def get_synsets(fpaths: Iterator[Union[str, Path]]) -> Dict:
     """Gets synsets with id as key and senses as values."""
     synsets_dict = {}
     for fp in fpaths:
-        print(f"Parsing {fp}.")
+        sys.stderr.write(f"Parsing {fp}.\n")
         xml_data = open(fp, 'rt').read()
         xml_parser = BeautifulSoup(xml_data, "lxml-xml")
         for synset in xml_parser.findAll('synset'):
@@ -30,13 +70,27 @@ def get_synsets(fpaths: Iterator[Union[str, Path]]) -> Dict:
     return synsets_dict
 
 
+def get_hyperstar_senses(fpaths: Iterator[Union[str, Path]]) -> List[dict]:
+    """Gets senses with their hypernyms."""
+    senses = []
+    for fp in fpaths:
+        sys.stderr.write(f"Parsing {fp}.\n")
+        with open(fp, 'rt') as fin:
+            for row in fin:
+                hyponym, hypernym = row.rstrip().split()
+                if senses and (hyponym == senses[-1]['content']):
+                    senses[-1]['hypernyms'].append({'content': hypernym})
+                else:
+                    senses.append({'content': hypernym,
+                                   'hypernyms': [{'content': hypernym}]})
+    return senses
+
 if __name__ == "__main__":
     synsets = get_synsets(sys.argv[1:])
-    print(f"Found {len(synsets)} synsets.")
+    sys.stderr.write(f"Found {len(synsets)} synsets.\n")
 
-    print(f"\nExamples:")
+    sys.stderr.write(f"\nExamples:\n")
     for i, s in enumerate(synsets.items()):
         if i > 2:
             break
-        print(s)
-
+        sys.stderr.write(f'{s}\n')
