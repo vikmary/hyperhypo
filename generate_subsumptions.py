5e79b09494a622a4851998006759b1d1b651ebed
Mariia Trofimova <mary.vikhreva@gmail.com>
2020-01-20 18:24:27 +0300

    feat: script for generating subsumptions

---
+++ generate_subsumptions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import csv
+import argparse
+from pathlib import Path
+from typing import Iterator, Tuple
+
+from utils import get_train_synsets, get_synsets
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train-data-paths', '-t', type=Path, nargs='+',
+                        help='path(s) to files with training data')
+    parser.add_argument('--out-path', '-o', type=Path,
+                        help='path to a output sumsamptions file')
+    parser.add_argument('--ruwordnet-dir', '-w', type=Path,
+                        help='path to a ruwordnet directory')
+    return parser.parse_args()
+
+
+def write_subsumptions(subsumptions: Iterator[Tuple[str, str]], fname: str) -> None:
+    with open(fname, 'wt') as f:
+        writer = csv.writer(f, dialect='excel-tab', lineterminator='\n')
+        for pair in subsumptions:
+            writer.writerow(pair)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    train_synsets = get_train_synsets(args.train_data_paths)
+
+    all_synsets = get_synsets(args.ruwordnet_dir.glob('synsets*'))
+
+    write_subsumptions(((s['content'].lower(), hyper_s['content'].lower())
+                        for synset in train_synsets.values()
+                        for s in synset['senses']
+                        for hyper_synset in synset['hypernyms']
+                        for hyper_s in all_synsets[hyper_synset['id']]['senses']),
+                       fname=args.out_path)
