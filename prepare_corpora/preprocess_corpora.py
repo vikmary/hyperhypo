37a528a2572755f9568b991de137547b9cedeb41
Mariia Trofimova <mary.vikhreva@gmail.com>
2020-01-29 11:45:35 +0300

    fix: preprocessing of news

--- prepare_corpora/preprocess_corpora.py
+++ prepare_corpora/preprocess_corpora.py
@@ -3,6 +3,7 @@
 
 import argparse
 import re
+import json
 from pathlib import Path
 
 from tqdm import tqdm
@@ -14,12 +15,14 @@ from utils import smart_open, count_lines, extract_zip, Sanitizer, Lemmatizer
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data-paths', '-p', type=Path, nargs='+',
-                        help='paths to a plain text files with corpora')
+    parser.add_argument('--data-path', '-p', type=Path,
+                        help='path to a plain text file with corpora')
     parser.add_argument('--max-lines', '-l', type=int, default=None,
                         help='take only first l lines of corpus')
     parser.add_argument('--min-tokens', '-t', type=int, default=4,
                         help='filter sentences with number of tokens less than t')
+    parser.add_argument('--news', '-n', action='store_true',
+                        help='whether the input corpus is in news format')
     parser.add_argument('--min-characters', '-c', type=int, default=20,
                         help='filter sentences with number of characters less than c')
     return parser.parse_args()
@@ -33,43 +36,46 @@ if __name__ == "__main__":
     lemmatizer = Lemmatizer()
 
     # extract zip files
-    data_paths = []
-    for fp in args.data_paths:
-        if '.zip' in fp.suffixes:
-            extracted_fpaths = extract_zip(fp)
-            data_paths.extend(extracted_fpaths)
-        else:
-            data_paths.append(fp)
+    if '.zip' in args.data_path.suffixes:
+        extracted_data_paths = extract_zip(args.data_path)
+    else:
+        extracted_data_paths = [args.data_path]
 
     if args.max_lines is None:
         print(f"Counting number of input lines.")
-        num_lines = sum(count_lines(fp) for fp in args.data_paths)
+        num_lines = sum(count_lines(fp) for fp in extracted_data_paths)
     else:
         num_lines = args.max_lines
 
     i = 0
     pbar = tqdm(total=num_lines, mininterval=10.)
-    for fp in data_paths:
-        if i >= num_lines:
-            break
-        base_name = fp.name.split('.')[0]
-        tokenized_outpath = fp.with_name('corpus.' + base_name + '.token.txt.gz')
-        lemmatized_outpath = fp.with_name(base_name + '.lemma.txt.gz')
-        print(f"Writing tokenized corpus to {tokenized_outpath}.")
-        print(f"Writing lemmatized corpus to {lemmatized_outpath}.")
 
-        with smart_open(fp, 'rt') as fin, \
-                smart_open(tokenized_outpath, 'wt') as f_tok, \
-                smart_open(lemmatized_outpath, 'wt') as f_lem:
-            for line in fin:
-                if i >= num_lines:
-                    break
-                i += 1
-                pbar.update(1)
-                line = sanitizer(line.strip())
-                if line:
-                    for sent in ru_sent_tokenize(line, SHORTENINGS, JOINING_SHORTENINGS,
-                                                 PAIRED_SHORTENINGS):
+    base_name = args.data_path.name.split('.')[0]
+    tokenized_outpath = args.data_path.with_name('corpus.' + base_name +
+                                                 '.token.txt.gz')
+    lemmatized_outpath = args.data_path.with_name(base_name + '.lemma.txt.gz')
+    print(f"Writing tokenized corpus to {tokenized_outpath}.")
+    print(f"Writing lemmatized corpus to {lemmatized_outpath}.")
+    with smart_open(tokenized_outpath, 'wt') as f_tok, \
+            smart_open(lemmatized_outpath, 'wt') as f_lem:
+        for fp in extracted_data_paths:
+            with smart_open(fp, 'rt') as fin:
+                for line in fin:
+                    if i >= num_lines:
+                        break
+                    i += 1
+                    pbar.update(1)
+                    line = sanitizer(line.strip())
+                    if args.news:
+                        try:
+                            sents = json.loads(line.split(maxsplit=1)[1])
+                        except:
+                            sents = []
+                            print("Failed to load sentences.")
+                    else:
+                        sents = ru_sent_tokenize(line, SHORTENINGS, JOINING_SHORTENINGS,
+                                                 PAIRED_SHORTENINGS)
+                    for sent in sents:
                         try:
                             if len(sent) < args.min_characters:
                                 continue
