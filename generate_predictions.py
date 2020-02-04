c3b617bf5fa2496a61690970b168b5920a055a58
Mariia Trofimova <mary.vikhreva@gmail.com>
2020-02-04 13:29:26 +0300

    fix: faster corpusindexed and lemmatize test words before looking them in the index

--- generate_predictions.py
+++ generate_predictions.py
@@ -3,6 +3,7 @@
 
 import sys
 import json
+import re
 import argparse
 from pathlib import Path
 from random import sample
@@ -11,6 +12,7 @@ from datetime import datetime
 from itertools import repeat
 from typing import List, Dict, Union, Tuple, Optional
 
+import hashedindex
 import torch
 from tqdm import tqdm
 from transformers import BertConfig, BertModel, BertTokenizer
@@ -19,7 +21,7 @@ from hybert import HyBert
 from dataset import HypoDataset
 from train import device, to_device
 from utils import get_test_senses, get_train_synsets, synsets2senses, get_wordnet_synsets
-from prepare_corpora.utils import smart_open
+from prepare_corpora.utils import smart_open, Lemmatizer, Sanitizer
 from postprocess_prediction import get_prediction
 
 
@@ -109,7 +111,10 @@ def load_candidates(fname: Union[str, Path]) -> Dict[str, List[str]]:
 
 
 class CorpusIndexed:
-    def __init__(self, index_path: Union[str, Path]) -> None:
+    def __init__(self,
+                 index_path: Union[str, Path],
+                 vocab: Optional[List[str]] = None) -> None:
+        self.vocab = vocab
         self.index_path = Path(index_path)
 
         base_name = self.index_path.name.rsplit('.json', 1)[0]
@@ -117,8 +122,9 @@ class CorpusIndexed:
         if '-head-' in base_name:
             base_name, num_sents = base_name.rsplit('-head-', 1)
             num_sents = int(num_sents)
+            print(f"Will load only {num_sents} sentences from corpus.")
         else:
-            num_sents = float('inf')
+            num_sents = sys.maxsize
         self.corpus_path = Path(self.index_path.with_name('corpus.' + base_name +
                                                           '.token.txt.gz'))
         if not self.corpus_path.exists():
@@ -127,32 +133,54 @@ class CorpusIndexed:
         if not self.corpus_path.exists():
             raise RuntimeError(f"corpus {self.corpus_path} doesn't exists")
 
-        print(f"Loading corpus from {self.corpus_path}.", file=sys.stderr)
-        self.corpus = [ln.strip()
-                       for i, ln in enumerate(smart_open(self.corpus_path, 'rt'))
-                       if i < num_sents]
-        print(f"Loading index from {self.index_path}.", file=sys.stderr)
-        self.idx = {w: idxs for w, idxs in json.load(open(self.index_path, 'rt')).items()}
+        self.corpus = self.load_corpus(self.corpus_path, num_sents)
+        self.idx = self.load_index(self.index_path, self.vocab)
+
+    @classmethod
+    def load_corpus(cls,
+                    path: Union[str, Path],
+                    num_sents: int = sys.maxsize) -> List[str]:
+        print(f"Loading corpus from {path}.", file=sys.stderr)
+        with smart_open(path, 'rt') as fin:
+            return [ln.strip() for _, ln in zip(range(num_sents), fin)]
+
+    @classmethod
+    def load_index(cls,
+                   path: Union[str, Path],
+                   vocab: Optional[List[str]] = None) -> hashedindex.HashedIndex:
+        print(f"Loading index from {path}.", file=sys.stderr)
+        index = hashedindex.HashedIndex()
+        if vocab is not None:
+            print(f"Will load only {len(vocab)} lemmas present in a vocab.")
+        for lemma, idxs in json.load(open(path, 'rt')).items():
+            if not vocab or (lemma in vocab):
+                index.add_term_occurence(lemma, idxs)
+        return index
 
     def get_contexts(self,
-                     word: str,
+                     lemma: str,
                      max_num_tokens: Optional[int] = None) -> List[Tuple[str, int]]:
-        if word not in self.idx:
-            print(f"Warning: word '{word}' not in index.", file=sys.stderr)
+        if lemma not in self.idx:
+            print(f"Warning: lemma '{lemma}' not in index.", file=sys.stderr)
             return []
-        sents = ((self.corpus[sent_idx].split(), pos) for sent_idx, pos in self.idx[word])
+        sents = ((self.corpus[sent_idx].split(), pos)
+                 for sent_idx, pos in self.idx.get_documents(lemma))
         if max_num_tokens is not None:
             sents = list(filter(lambda s_pos: len(s_pos[0]) < max_num_tokens, sents))
             if not sents:
                 w_size = max_num_tokens // 2
                 sents = ((self.corpus[s_id].split()[pos - w_size: pos + w_size],
                           w_size - max(w_size - pos, 0))
-                         for s_id, pos in self.idx[word])
+                         for s_id, pos in self.idx.get_documents(lemma))
         return list(sents)
 
 
 if __name__ == "__main__":
     args = parse_args()
+
+    sanitizer = Sanitizer(filter_stresses=True, filter_empty_brackets=True)
+    tokenizer = re.compile(r"[\w']+|[^\w ]")
+    lemmatizer = Lemmatizer()
 
     # load wordnet and word to get prediction for
     if args.is_train_format:
@@ -160,6 +188,9 @@ if __name__ == "__main__":
         test_senses = [s['content'].lower() for s in synsets2senses(test_synsets)]
     else:
         test_senses = [s['content'].lower() for s in get_test_senses([args.data_path])]
+    test_lemmas = [' '.join(lemmatizer(t)
+                            for t in tokenizer.findall(sanitizer(s).lower()))
+                   for s in test_senses]
     synsets = get_wordnet_synsets(args.wordnet_dir.glob('synsets.*'))
 
     # load fallback predictions if needed
@@ -187,7 +218,7 @@ if __name__ == "__main__":
     model.eval()
 
     # get corpus with contexts and it's index
-    corpus = CorpusIndexed(args.index_path)
+    corpus = CorpusIndexed(args.index_path, vocab=test_lemmas)
 
     # generating output fila name
     now = datetime.now()
@@ -197,8 +228,8 @@ if __name__ == "__main__":
 
     n_skipped = 0
     with open(out_pred_path, 'wt') as f_pred:
-        for word in tqdm(test_senses):
-            contexts = corpus.get_contexts(word, max_num_tokens=250)
+        for word, lemma in tqdm(zip(test_senses, test_lemmas)):
+            contexts = corpus.get_contexts(lemma, max_num_tokens=250)
             if not contexts:
                 n_skipped += 1
                 if word not in fallback_preds:
