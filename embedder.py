cc170aa803f6ca0d9072e110bc24e99f80cd80a9
Mariia Trofimova <mary.vikhreva@gmail.com>
2020-02-06 21:35:42 +0300

    refactor: add debug output to ./embedder.py

--- embedder.py
+++ embedder.py
@@ -25,19 +25,22 @@ def get_word_embeddings(words: List[str],
             print(f"subword_ids('{w}') = {subword_ids[-1]}")
             print(f'{[tokenizer._convert_id_to_token(s) for s in subword_ids[-1]]}')
         max_len = max_len if max_len > num_subwords else num_subwords
+    # subword_ids, subword_masks: [num_words, max_len]
     subword_ids = torch.tensor([sw_list + [-1] * (max_len - len(sw_list))
                                 for sw_list in subword_ids])
     subword_masks = torch.tensor([m + [0.] * (max_len - len(m))
                                  for m in subword_masks])
+    # subword_sizes: [num_words]
     subword_sizes = torch.sum(subword_masks, 1)
     if debug:
         print(subword_sizes)
+    # emb_mat[subword_ids]: [num_words, max_len, emb_size]
     return torch.sum(emb_mat[subword_ids] * subword_masks.unsqueeze(2), axis=1) \
         / subword_sizes.unsqueeze(1)
 
 
 if __name__ == "__main__":
-    model_path = Path('/home/arcady/data/models/rubert_cased_L-12_H-768_A-12_v2/')
+    model_path = Path('/home/hdd/models/rubert_v2/rubert_cased_L-12_H-768_A-12_v2/')
 
     config = BertConfig.from_pretrained(model_path / 'bert_config.json')
     tokenizer = BertTokenizer.from_pretrained(model_path, do_lower_case=False)
@@ -47,5 +50,12 @@ if __name__ == "__main__":
 
     res = get_word_embeddings(['мыть', 'мама'], emb_mat, tokenizer=tokenizer)
     print(res, res.shape)
-    res = get_word_embeddings(['Литва', 'Литовская Республика', 'ывралыва'], emb_mat, tokenizer=tokenizer)
-    print(res, res.shape)
+    res1 = get_word_embeddings(['Литва', 'Литовская Республика', 'ывралыва'], emb_mat,
+                              tokenizer=tokenizer)[1]
+    print(res1, res1.shape)
+
+    res2 = get_word_embeddings(['Литовская', 'Республика'], emb_mat,
+                               tokenizer=tokenizer).sum(dim=0) / 2
+    print(res2.shape)
+
+    print("Difference between embeddings of 'Литовская Республика':", (res1 - res2).norm())
