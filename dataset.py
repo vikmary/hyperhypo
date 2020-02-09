b818a67fca0145c01fe9cea151b4a8a0ed10bf42
mu-arkhipov <arkhipov@yahoo.com>
2020-02-09 16:38:54 +0300

    feat: make dataset indexable, add max len, return distribution instead of idxs

--- dataset.py
+++ dataset.py
@@ -4,30 +4,47 @@
 import json
 from itertools import chain
 from pathlib import Path
-from random import randint, sample
-from typing import Union, List, Tuple, Optional
+from random import randint, sample, shuffle
+from typing import Union, List, Tuple, Optional, Iterable
 
 import torch
 from torch.utils.data import DataLoader
-from torch.utils.data.dataset import IterableDataset
+from torch.utils.data.dataset import Dataset
 from transformers import BertTokenizer
 
 
-class HypoDataset(IterableDataset):
+class HypoDataset(Dataset):
     def __init__(self,
                  tokenizer: BertTokenizer,
                  corpus_path: Union[str, Path],
                  hypo_index_path: Union[str, Path],
                  train_set_path: Union[str, Path],
                  hypernym_list: List[str],
-                 debug: bool = False):
+                 debug: bool = False,
+                 predict_all_hypes=True,
+                 max_len=128):
         self.tokenizer = tokenizer
         self.corpus = self._read_corpus(corpus_path)
         self.hypo_index = self._read_json(hypo_index_path)
-        self.train_set = self._read_json(train_set_path)
+
+        train_set = self._read_json(train_set_path)
+        self.train_set = []
+        not_in_index_list = []
+        for hypos, *hypes in train_set:
+            for hypo in hypos:
+                if hypo in self.hypo_index:
+                    self.train_set.append([hypo, *hypes])
+                else:
+                    not_in_index_list.append(hypo)
+        not_in_index_percent = len(not_in_index_list) / (len(self.train_set) + len(not_in_index_list))
+        print(f'{not_in_index_percent:.2f} hyponyms are not found in the index')
+        # print(f'Not found hyponyms:\n{not_in_index_list}')
+
         self.hypernym_to_idx = {hype: n for n, hype in enumerate(hypernym_list)}
         self.hypernym_list = hypernym_list
         self.debug = debug
+        self.predict_all_hypes = predict_all_hypes
+        self.max_len = max_len
 
     @classmethod
     def _read_json(cls, hypo_index_path: Union[str, Path]):
@@ -39,55 +56,74 @@ class HypoDataset(IterableDataset):
         with open(corpus_path, encoding='utf8') as handle:
             return handle.readlines()
 
-    def __iter__(self):
-        while True:
-            train_ind = randint(0, len(self.train_set) - 1)
-            hypos, hypes, hype_hypes = self.train_set[train_ind]
-            hypos = [h.lower() for h in hypos]
-            hypos_in_index = [h for h in hypos if h in self.hypo_index]
+    def __len__(self):
+        return len(self.train_set)
 
-            if not hypos_in_index:
-                if self.debug:
-                    print(f'Empty index for hypos: {hypos}')
-                continue
-            if self.debug and len(hypos) != len(hypos_in_index):
-                print(f'Some hypos are lost. Original: {hypos},'
-                      f' In index: {hypos_in_index}')
+    def __getitem__(self, item):
+        hypo, hypes, hype_hypes = self.train_set[item]
+        hypo = hypo.lower()
+        all_hypes = list(chain(*(hypes + hype_hypes)))
+        sent_idx, in_sent_start, in_sent_end = sample(self.hypo_index[hypo], 1)[0]
+        sent_toks = self.corpus[sent_idx].split()
 
-            hypo = sample(hypos_in_index, 1)[0]
-            all_hypes = list(chain(*(hypes + hype_hypes)))
+
+        sent_toks =  sent_toks
+        subword_idxs, hypo_mask, subtok_start, subtok_end = \
+            self._get_indices_and_masks(sent_toks, in_sent_start, in_sent_end)
+        if len(subword_idxs) > self.max_len:
+            half_max_len = self.max_len // 2
+            new_start = max(0, subtok_start - half_max_len)
+            subword_idxs = subword_idxs[new_start: new_start + self.max_len]
+            hypo_mask = hypo_mask[new_start: new_start + self.max_len]
+        cls_idx, sep_idx = self.tokenizer.convert_tokens_to_ids(['[CLS]', '[SEP]'])
+        subword_idxs = [cls_idx] + subword_idxs + [sep_idx]
+        hypo_mask = [0.0] + hypo_mask + [0.0]
+        if sum(hypo_mask) == 0.0:
+            print('Damn!')
+
+        subword_idxs = subword_idxs[:self.max_len]
+        hypo_mask = hypo_mask[:self.max_len]
+        hype_prob = [0.0] * len(self.hypernym_list)
+        if self.predict_all_hypes:
+            hype_idxs = [self.hypernym_to_idx[hype] for hype in all_hypes]
+            single_hype_prob = 1 / len(hype_idxs)
+            for hype_idx in hype_idxs:
+                hype_prob[hype_idx] = single_hype_prob
+        else:
             hype = sample(all_hypes, 1)[0]
             hype_idx = self.hypernym_to_idx[hype]
-            if len(self.hypo_index[hypo]) == 0:
-                continue
-            sent_idx, in_sent_hypo_idx = sample(self.hypo_index[hypo], 1)[0]
-            sent_toks = self.corpus[sent_idx].split()
-            sent_toks = ['[CLS]'] + sent_toks + ['[SEP]']
-            subword_idxs, hypo_mask = self._get_indices_and_masks(sent_toks, in_sent_hypo_idx)
-            yield subword_idxs, hypo_mask, hype_idx
+            hype_prob[hype_idx] = 1.0
+        return subword_idxs, hypo_mask, hype_prob
 
-    def _get_indices_and_masks(self, sent_tokens: List[str], in_sent_hypo_idx: int):
+    def _get_indices_and_masks(self,
+                               sent_tokens: List[str],
+                               in_sent_start: int,
+                               in_sent_end: int):
         sent_subword_idxs = []
         sent_subwords = []
         sent_hypo_mask = []
         for n, tok in enumerate(sent_tokens):
+            if n == in_sent_start:
+                new_in_sent_start = len(sent_subwords)
             subtokens = self.tokenizer.tokenize(tok)
             sent_subwords.extend(subtokens)
             subtok_idxs = self.tokenizer.convert_tokens_to_ids(subtokens)
             sent_subword_idxs.extend(subtok_idxs)
-            # NOTE: + 1 because of [CLS] token in the beginning
-            mask_value = float(n == in_sent_hypo_idx + 1)
+            # NOTE: absence of + 1 because absence of [CLS] token in the beginning
+            mask_value = float(in_sent_start <= n < in_sent_end)
             sent_hypo_mask.extend([mask_value] * len(subtok_idxs))
-        return sent_subword_idxs, sent_hypo_mask
+            if n == in_sent_end - 1:
+                new_in_sent_end = len(sent_subwords) + 1
+        return sent_subword_idxs, sent_hypo_mask, new_in_sent_start, new_in_sent_end
 
     def get_all_hypo_samples(self,
                              hypo: str) \
                              -> Tuple[List[Union[torch.Tensor, List[int], List[float]]]]:
         hypo_mentions = self.hypo_index[hypo]
         sents_indices, sents_hypo_masks = [], []
-        for sent_idx, in_sent_hypo_idx in hypo_mentions:
+        for sent_idx, in_sent_start, in_sent_end  in hypo_mentions:
             sent_toks = self.corpus[sent_idx].split()
-            subword_idxs, hypo_mask = self._get_indices_and_masks(sent_toks, in_sent_hypo_idx)
+            subword_idxs, hypo_mask = self._get_indices_and_masks(sent_toks, in_sent_start, in_sent_end)
             sents_indices.append(subword_idxs)
             sents_hypo_masks.append(hypo_mask)
         batch_parts = self.torchify_and_pad(sents_indices, sents_hypo_masks)
@@ -98,7 +134,7 @@ class HypoDataset(IterableDataset):
     def torchify_and_pad(cls,
                          sents_indices: List[List[int]],
                          sents_masks: List[List[float]],
-                         hype_idxs: Optional[List[int]] = None) -> Tuple[torch.Tensor]:
+                         hype_one_hot: Optional[List[List[float]]] = None) -> Tuple[torch.Tensor]:
         batch_size = len(sents_indices)
         max_len = max(len(idx) for idx in sents_indices)
         padded_indices = torch.zeros(batch_size, max_len, dtype=torch.long)
@@ -112,9 +148,9 @@ class HypoDataset(IterableDataset):
             padded_indices[n, :up_to] = sent_idxs
             padded_mask[n, :up_to] = sent_mask
             padded_att_mask[n, :up_to] = 1.0
-        if hype_idxs:
-            hype_idxs = torch.tensor(hype_idxs)
-            return padded_indices, padded_mask, padded_att_mask, hype_idxs
+        if hype_one_hot:
+            hype_one_hot = torch.tensor(hype_one_hot)
+            return padded_indices, padded_mask, padded_att_mask, hype_one_hot
         else:
             return padded_indices, padded_mask, padded_att_mask
 
@@ -141,8 +177,8 @@ if __name__ == '__main__':
     # hypo_index_path = 'sample_data/tst_index.json'
     # train_set_path = 'sample_data/tst_train.json'
     data_path = Path('/home/hdd/data/hypernym/')
-    corpus_path = data_path / 'corpus.wikipedia-ru-2018-sample.token.txt'
-    hypo_index_path = data_path / 'index.train.wikipedia-ru-2018-sample.json'
+    corpus_path = data_path / 'corpus.news_dataset-sample.token.txt'
+    hypo_index_path = data_path / 'index.train.news_dataset-sample.json'
     train_set_path = data_path / 'train.cased.json'
 
     model_path = Path('/home/hdd/models/rubert_cased_L-12_H-768_A-12_v2/')
@@ -159,9 +195,22 @@ if __name__ == '__main__':
 
     sentence_indices, sentence_hypo_mask, hype_idx = next(iter(ds))
     print(f'Indices: {sentence_indices}\n'
-          f'Hypo Mask: {sentence_hypo_mask}')
-    print('Note that dataset samples randomly form all possible samples')
+          f'Hypo Mask: {sentence_hypo_mask}\n'
+          f'Hype idx: {hype_idx}')
+    print('=' * 20)
+    print('Returning all hypes')
 
+    ds = HypoDataset(tokenizer,
+                     corpus_path,
+                     hypo_index_path,
+                     train_set_path,
+                     hype_list,
+                     predict_all_hypes=True)
+
+    sentence_indices, sentence_hypo_mask, hype_idxs = next(iter(ds))
+    print(f'Indices: {sentence_indices}\n'
+          f'Hypo Mask: {sentence_hypo_mask}\n'
+          f'hype_idxs: {hype_idxs}')
     print('=' * 20)
 
     dl = DataLoader(ds, batch_size=2, collate_fn=batch_collate)
@@ -180,5 +229,5 @@ if __name__ == '__main__':
           f'Mask: {masks}\n'
           f'Attention masks: {att_masks}')
 
-# TODO: add repetion of the same hypo
+# TODO: add repetition of the same hypo
 # TODO: separate class
