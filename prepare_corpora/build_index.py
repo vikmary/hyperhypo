52faca152a01795a8949e2bdf8f137e9a9995ccc
Mariia Trofimova <mary.vikhreva@gmail.com>
2020-02-23 13:35:38 +0300

    fix: adap build_index to new preprocessor format

--- prepare_corpora/build_index.py
+++ prepare_corpora/build_index.py
@@ -16,6 +16,8 @@ def parse_args():
                         help='path to lemmatized corpora')
     parser.add_argument('--train-paths', '-t', type=Path, nargs='+',
                         help='path(s) to text file(s) with training data')
+    parser.add_argument('--out-prefix', type=str,
+                        help='prefix of the output file')
     parser.add_argument('--synset-info-paths', '-s', type=Path, nargs='+',
                         help='paths to synset info for training data')
     parser.add_argument('--max-lines', '-l', type=int, default=None,
@@ -28,7 +30,9 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    preprocessor = TextPreprocessor(filter_stresses=True,
+    # TODO: add parsing udpipe suffix
+    preprocessor = TextPreprocessor('regexp+pymorphy',
+                                    filter_stresses=True,
                                     filter_empty_brackets=True,
                                     lowercase=True,
                                     lemmatize=True)
@@ -37,7 +41,7 @@ if __name__ == "__main__":
     for synset_id, synset in get_train_synsets(args.train_paths, args.synset_info_paths)\
             .items():
         for sense in synset['senses']:
-            hypo = preprocessor(sense['content'])
+            _, hypo = preprocessor(sense['content'])
             if not hypo:
                 print(f"Skipping hyponym {hypo}, because it's lemma is empty.")
             else:
@@ -51,7 +55,7 @@ if __name__ == "__main__":
     base_name = args.data_path.name.split('.')[0]
     if args.max_lines is not None:
         base_name += f'-head-{args.max_lines}'
-    out_path = args.data_path.with_name('index.train.' + base_name + '.json')
+    out_path = args.data_path.with_name(args.out_prefix + base_name + '.json')
 
     print(f"Writing training index to {out_path}.")
     json.dump(inverted_index, open(out_path, 'wt'), indent=2, ensure_ascii=False)
